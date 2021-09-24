2e0c30c141b9aed30df7464da1489ee07b9a95b4
Niels Klijn <niels@klijn.engineer>
2021-09-24 18:36:56 +0100

    add documentation to IHttpClientWrapper

--- PeterStroopwafel.Bestellen/Ordering/Services/IHttpClientWrapper.cs
+++ PeterStroopwafel.Bestellen/Ordering/Services/IHttpClientWrapper.cs
@@ -5,8 +5,18 @@ namespace Ordering.Services
 {
     public interface IHttpClientWrapper
     {
+        
+        /// <summary>
+        /// Get request
+        /// </summary>
+        /// <exception cref="HttpRequestException">When the HTTP status code is not successful</exception>
         HttpContent Get(HttpRequestMessage request);
 
+        
+        /// <summary>
+        /// POST request
+        /// </summary>
+        /// <exception cref="HttpRequestException">When the HTTP status code is not successful</exception>
         HttpResponseMessage Post(Uri requestUri, HttpContent content);
     }
 }
